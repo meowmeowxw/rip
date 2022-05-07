b03ee3db54d0af5fb103e91b81db50d934debd62
meowmeowxw <giovanni.disanti@protonmail.com>
2022-05-07 11:05:33 +0200

    Remove notifications polling

--- public/js/public.js
+++ public/js/public.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
             type: "GET",
             data: {'name': query},
             success: function (data) {
-                if (data != '') {
+                if (data !== '') {
                     $('#product_list').html(data);
                 }
             }
@@ -39,59 +39,4 @@ $(document).ready(function () {
             $('#product_list').removeClass('show');
         }, 100);
     });
-
-    function askForApproval(title, text, url) {
-        if(Notification.permission === "granted") {
-            createNotification(title, text, '/img/ripperoni-1.png', url);
-        }
-        else {
-            Notification.requestPermission(permission => {
-                if(permission === 'granted') {
-                    createNotification(title, text, baseUrl + '/img/ripperoni-1.png', url)
-                }
-            });
-        }
-    }
-
-    function createNotification(title, text, icon, url) {
-        console.log('here');
-        const noti = new Notification(title, {
-            body: text,
-            icon
-        });
-
-        noti.onclick = function() {
-            if (url) {
-                window.location.href = url;
-            }
-        }
-    }
-
-    // askForApproval('Once', 'Once');
-    setInterval(function () {
-        $.ajax({
-            url: baseUrl + "notifications",
-            type: "GET",
-            success: function (data) {
-                messages = JSON.parse(data)
-                if (messages.length !== 0) {
-                    messages.forEach(function(message) {
-                        data = message.data;
-                        console.log(message);
-                        if (data.status) {
-                            askForApproval('Order Status Updated', `Order n. ${data.order}: ${data.status}`, data.url);
-                        }
-                        if (data.type === 'forCustomer'){
-                            askForApproval('Checkout Confirmed', `The new order it's added lo the list (id ${data.order})`, data.url);
-                        }
-                        if (data.type === 'forSeller'){
-                            askForApproval('New Order Received', `n. ${data.order}`, data.url);
-                        }
-                    });
-                    // askForApproval('10', 'ciao');
-                }
-            }
-        })
-    }, 1000);
-
 });
